9ecaadf2f515140198ac0c878730376fccd16599
midhilesh <chintu.midhilesh@gmail.com>
2021-06-10 04:40:17 +0530

    Added All Crud Operations using Class Based Views

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
-from .views import Tasklist
+from django.views.generic.edit import DeleteView
+from .views import TaskCreate, TaskDetail, TaskUpdate, Tasklist,DeleteView
+
 urlpatterns = [
-    path('',Tasklist.as_view(),name='tasks')
+    path('',Tasklist.as_view(),name='tasks'),
+    path('task/<int:pk>/',TaskDetail.as_view(),name='task'),
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
+    path('task-update/<int:pk>', TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<int:pk>', DeleteView.as_view(), name='task-delete'),
 ]
